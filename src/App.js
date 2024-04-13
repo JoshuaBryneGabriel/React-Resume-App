e6ba65c8235366183d8fd80d034aebd0a53ba4a1
Joshua Bryne Gabriel <joshuagabriel0524@gmail.com>
2024-04-13 10:57:12 +0800

    UI and Design Changes

--- src/App.js
+++ src/App.js
@@ -2,44 +2,38 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <img src={profilePic} alt="Profile" style={{ width: '100px', height: '100px', marginRight: '20px' }} />
-        <h1>Joshua Gabriel</h1>
-        <h2>Software Developer</h2>
-      </header>
-      <section>
-        <h3>About Me</h3>
-        <p>I am a Computer Science Graduate of the University of The East with 4 months Project Based Contract experience as a Software Developer</p>
-      </section>
-      <section>
-        <h3>Skills</h3>
-        <ul>
-          <li>React</li>
-          <li>Node.js</li>
-          <li>MongoDB</li>
-        </ul>
-      </section>
-      <section>
-        <h3>Experience</h3>
-        <h4>Company Name</h4>
-        <p>Role: Full Stack Developer</p>
-        <p>Duration: Jan 2020 - Present</p>
-        <p>Description: Developed and maintained web applications using React, Node.js, and MongoDB.</p>
-      </section>
-      <section>
-        <h3>Education</h3>
-        <h4>University Name</h4>
-        <p>Degree: Bachelor's in Computer Science</p>
-        <p>Year: 2016 - 2020</p>
-      </section>
-      <footer>
-        <p>Contact: johndoe@example.com</p>
-        <p>LinkedIn: <a href="https://www.linkedin.com/in/johndoe/">https://www.linkedin.com/in/johndoe/</a></p>
-      </footer>
-    </div>
+  <div className= "app" style={{ backgroundColor: 'teal', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)' }}>
+    <header className="App-header">
+      {/* <img src={profilePic} alt="Profile" style={{ width: '100px', height: '100px', marginRight: '20px' }} /> */}
+      <h1>Joshua Gabriel</h1>
+      <h2>Software Developer</h2>
+    </header>
+    <section style={{ backgroundColor: 'cyan', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)' }}>
+      <h3>About Me</h3>
+      <p>I am a Computer Science Graduate of the University of The East with 4 months Project Based Contract experience as a Software Developer</p>
+    </section>
+    <section style={{ backgroundColor: 'cyan', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)' }}>
+      <h3>Skills</h3>
+      <ul style={{ listStyleType: 'none', padding: 0 }}>
+        <li>C#</li>
+        <li>.NET Framework</li>
+        <li>ASP.NET</li>
+        <li>HTML</li>
+        <li>Node.js</li>
+        <li>MongoDB</li>
+      </ul>
+    </section>
+    <section style={{ backgroundColor: 'cyan', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)' }}>
+      <h3>Experience</h3>
+      <h4>Company Name</h4>
+      <p>Role: Software Developer</p>
+      <p>Duration: Dec 2023 - April 2024</p>
+      <p>Description: Developed Tickets Application using C# Windows Forms with .NET Framework</p>
+    </section>
+  </div>
   );
 }
 
 export default App;
+
 
