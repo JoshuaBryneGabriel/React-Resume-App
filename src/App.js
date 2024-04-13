1a1d33cfcda42aa75b2e12ec79eca900825bc2a0
Joshua Bryne Gabriel <joshuagabriel0524@gmail.com>
2024-04-13 11:15:52 +0800

    Minor Adjustments to Info

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ function App() {
     </header>
     <section style={{ backgroundColor: 'cyan', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)' }}>
       <h3>About Me</h3>
-      <p>I am a Computer Science Graduate of the University of The East with 4 months Project Based Contract experience as a Software Developer</p>
+      <p>I am a Computer Science Graduate of the University of The East with 4 months Project Based Contract experience as a Software Developer,
+      now I am looking for a full-time job as a Software Developer to further enhance my skills and knowledge in my field of study as i am a fast learner,
+      hardworking and can work under pressure. I am also a team player and looking to work with experienced individuals, I am also willing to learn new technologies in a fast paced manner
+      in a short amount of time thanks to the advent of AI to help accelerate my learning and to be able to contribute to the company and to be able to help the company grow and succeed.
+      </p>
     </section>
     <section style={{ backgroundColor: 'cyan', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)' }}>
       <h3>Skills</h3>
@@ -19,16 +23,18 @@ function App() {
         <li>.NET Framework</li>
         <li>ASP.NET</li>
         <li>HTML</li>
-        <li>Node.js</li>
+        <li>JavaScript</li>
+        <li>Jquery</li>
+        <li>Python</li>
         <li>MongoDB</li>
       </ul>
     </section>
     <section style={{ backgroundColor: 'cyan', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)' }}>
       <h3>Experience</h3>
-      <h4>Company Name</h4>
+      <h4>Cario - (Smarter Freight Technologies)</h4>
       <p>Role: Software Developer</p>
       <p>Duration: Dec 2023 - April 2024</p>
-      <p>Description: Developed Tickets Application using C# Windows Forms with .NET Framework</p>
+      <p>Description: Developed Tickets Application using C# Windows Forms with .NET Framework, Handled SQL operations using MS-SQL and a short introductory in Angular Web Development</p>
     </section>
   </div>
   );
